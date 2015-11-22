7f066447db5f21c2636d580f18a031f8e62dcf72
arafat <mazharul0904055@yahoo.com>
2015-11-22 16:28:39 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mazharulislam/android_animation/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.mazharulislam.android_animation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView sun = (ImageView) findViewById(R.id.sun);
+        Animation sunRise = AnimationUtils.loadAnimation(this, R.anim.sun_rise);
+        sun.startAnimation(sunRise);
+        sunRise.setRepeatCount(Animation.INFINITE);
+        sunRise.setRepeatMode(Animation.INFINITE);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
